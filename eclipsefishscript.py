4e04e0d7069b432bc6ff9efc34a860e403e997cb
OdeMillington <76479376+OdeMillington@users.noreply.github.com>
2021-11-19 21:20:52 -0400

    Updated code
    Updated code to catch faults in the OCR

--- eclipsefishscript.py
+++ eclipsefishscript.py
@@ -9,6 +9,19 @@ pytesseract.pytesseract.tesseract_cmd = (
     r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 )
 
+#
+# def afk_avoid():
+#     screenshot2 = ImageGrab.grab(bbox=)
+#     screenshot2.save("AFK_math.png")
+#     text2 = str(pytesseract.image_to_string("AFK_math.png", config='--psm 7')).split()
+#     if "AFK" in text2:
+#         keyboard.press_and_release("k")
+#         time.sleep(2)
+#         keyboard.press_and_release("a")
+#         keyboard.press("a")
+#         time.sleep(0.1)
+#         keyboard.press()
+#
 
 while True:
     screenshot = ImageGrab.grab(bbox=(500, 0, 1920, 110))
@@ -16,7 +29,7 @@ while True:
     text = str(pytesseract.image_to_string("ECLIPSE.png", config='--psm 7')).split()
     print(text)
 
-    if "reel" in text:
+    if "reel" in text or "reel!" in text:
         keyboard.press_and_release("k")
         keyboard.press_and_release("i")
         mouse.move(865, 433, duration=0.5)
